9ae1beb8bd66aaad02b034bba26aed91a1046543
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2023-09-03 20:28:57 -0300

    feature/home/component: show list of posts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  posts = [
+    {
+      title: 'Teste 1',
+      text: 'Corpo do texto do primeiro post'
+    },
+    {
+      title: 'Teste 2',
+      text: 'Corpo do texto do segundo post'
+    },
+    {
+      title: 'Teste 3',
+      text: 'Teste de corpo de texto'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
