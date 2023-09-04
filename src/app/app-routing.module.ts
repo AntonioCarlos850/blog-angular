362a93011b37db78c47b31589ad7ff7a4ef5004e
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2023-09-04 00:44:39 -0300

    feature/app/routing: edit and delete routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CreateComponent } from './components/forms/create/create.component';
+import { DeleteComponent } from './components/forms/delete/delete.component';
+import { EditComponent } from './components/forms/edit/edit.component';
 
 const routes: Routes = [
   {
@@ -11,6 +13,14 @@ const routes: Routes = [
   {
     path: 'create',
     component: CreateComponent
+  },
+  {
+    path: 'delete/:id',
+    component: DeleteComponent
+  },
+  {
+    path: 'edit/:id',
+    component: EditComponent
   }
 ];
 
